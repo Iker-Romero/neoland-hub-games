59f8bbc965ff69ca8d4b4ca3f1bdcb6864361704
Iker-Romero <ikersoftdev@gmail.com>
2022-10-15 15:12:13 +0200

    tic-tac-toe grid layout replaced by a table layout

--- components/tic-tac-toe/tic-tac-toe.js
+++ components/tic-tac-toe/tic-tac-toe.js
@@ -1,18 +1,42 @@
 export const ticTacToe = () => {
+  // const template = `
+  // <main>
+  //   <h1>⭕ 3 EN RAYA ❌</h1>
+  //   <div class="squares-container">
+  //     <div class="square" id="square1"></div>
+  //     <div class="square" id="square2"></div>
+  //     <div class="square" id="square3"></div>
+  //     <div class="square" id="square4"></div>
+  //     <div class="square" id="square5"></div>
+  //     <div class="square" id="square6"></div>
+  //     <div class="square" id="square7"></div>
+  //     <div class="square" id="square8"></div>
+  //     <div class="square" id="square9"></div>
+  //   </div>
+  // </main>
+  // `
   const template = `
   <main>
     <h1>⭕ 3 EN RAYA ❌</h1>
-    <div class="squares-container">
-      <div class="square" id="square1"></div>
-      <div class="square" id="square2"></div>
-      <div class="square" id="square3"></div>
-      <div class="square" id="square4"></div>
-      <div class="square" id="square5"></div>
-      <div class="square" id="square6"></div>
-      <div class="square" id="square7"></div>
-      <div class="square" id="square8"></div>
-      <div class="square" id="square9"></div>
-    </div>
+    <table class="squares-container">
+      <tbody>
+        <tr>
+          <td class="square" id="square1"></td>
+          <td class="square" id="square1"></td>
+          <td class="square" id="square1"></td>
+        </tr>
+        <tr>
+          <td class="square" id="square1"></td>
+          <td class="square" id="square1"></td>
+          <td class="square" id="square1"></td>
+        </tr>
+        <tr>
+          <td class="square" id="square1"></td>
+          <td class="square" id="square1"></td>
+          <td class="square" id="square1"></td>
+        </tr>
+      </tbody>
+    </table>
   </main>
   `
   const target = document.querySelector("nav");
