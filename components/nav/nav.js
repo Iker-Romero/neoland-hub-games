9a77f6564bbbcab7cc7454280aee9c5ee0147151
Iker-Romero <ikersoftdev@gmail.com>
2022-10-15 14:31:12 +0200

    home button functionality

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,9 +1,17 @@
+import { hub } from "../hub/hub";
+
 export const nav = () => {
   const template = `
   <nav>
-    <span>HUB</span>
+    <button class="hub-button">HUB</button>
   </nav>
   `;
   const target = document.querySelector("#app");
   target.insertAdjacentHTML("afterbegin", template);
+
+  const hubButton = document.querySelector(".hub-button");
+  hubButton.addEventListener("click", () => {
+    document.querySelector("main").remove();
+    hub();
+  });
 };
