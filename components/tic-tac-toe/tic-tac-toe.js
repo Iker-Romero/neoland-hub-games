fcf62cc6503ccda6a7be56aca4f8797b053ca9ae
Iker-Romero <ikersoftdev@gmail.com>
2022-10-15 21:26:33 +0200

    tic-tac-toe computer moves and check winner (in process)

--- components/tic-tac-toe/tic-tac-toe.js
+++ components/tic-tac-toe/tic-tac-toe.js
@@ -1,20 +1,4 @@
 export const ticTacToe = () => {
-  // const template = `
-  // <main>
-  //   <h1>⭕ 3 EN RAYA ❌</h1>
-  //   <div class="squares-container">
-  //     <div class="square" id="square1"></div>
-  //     <div class="square" id="square2"></div>
-  //     <div class="square" id="square3"></div>
-  //     <div class="square" id="square4"></div>
-  //     <div class="square" id="square5"></div>
-  //     <div class="square" id="square6"></div>
-  //     <div class="square" id="square7"></div>
-  //     <div class="square" id="square8"></div>
-  //     <div class="square" id="square9"></div>
-  //   </div>
-  // </main>
-  // `
   const template = `
   <main>
     <h1>⭕ 3 EN RAYA ❌</h1>
@@ -43,31 +27,26 @@ export const ticTacToe = () => {
   const target = document.querySelector("nav");
   target.insertAdjacentHTML("afterend", template);
 
-  const squares = document.querySelectorAll(".square");
+  const squaresNodeList = document.querySelectorAll(".square");
 
   let userMoves = 3;
 
-  let computerMoves = 3;
-
-  const computerMove = () => {
-    const availableSquares = squares.filter((square) => {
-      return square.innerHTML === "";
-    });
-    const randomSquare =
-      availableSquares[Math.floor(Math.random() * availableSquares.length)];
-    randomSquare.innerHTML = "❌";
-  };
-
-  squares.forEach((square) => {
+  squaresNodeList.forEach((square) => {
     square.addEventListener("click", () => {
-      console.log("FUNCIONANDO");
-      if (square.innerHTML === "⭕") {
+
+      console.log(userMoves)
+
+      if (square.innerHTML === "⭕" && userMoves === 0) {
         square.innerHTML = "";
         userMoves++;
       } else if (userMoves > 0) {
         if (square.innerHTML === "") {
           square.innerHTML = "⭕";
           userMoves--;
+          computerMove();
+          if (winCheck("⭕")  === true) {
+            alert("¡ENHORABUENA HAS GANADO!");
+          }
         } else {
           alert("Esta casilla ya está ocupada.");
         }
@@ -79,5 +58,83 @@ export const ticTacToe = () => {
     });
   });
 
-  
+  let computerMoves = 3;
+
+  // Convertimos el nodeList a un array iterable, para que no solo lo sea con forEach() y poder usar el metodo filter()
+  const squares = Array.from(squaresNodeList);
+
+  const computerMove = () => {
+    if (computerMoves > 0) {
+      const availableSquares = squares.filter((square) => {
+        return square.innerHTML === "";
+      });
+      const randomSquare =
+        availableSquares[Math.floor(Math.random() * availableSquares.length)];
+      randomSquare.innerHTML = "❌";
+    } else if (winCheck("❌")) {
+      alert("El rival ha ganado.");
+    } else {
+      const ownSquares = squares.filter((square) => {
+        return square.innerHTML === "❌";
+      })
+      const takeRandom = ownSquares[Math.floor(Math.random() * ownSquares.length)];
+      takeRandom.innerHTML = "";
+      computerMoves++;
+    }
+  };
+
+  const winCheck = (player) => {
+    console.log("WINCHECK");
+
+    const combinations = [
+      [1, 1, 1,
+      0, 0, 0,
+      0, 0, 0],
+      [0, 0, 0,
+      1, 1, 1,
+      0, 0, 0],
+      [0, 0, 0,
+      0, 0, 0,
+      1, 1, 1],
+      [1, 0, 0,
+      1, 0, 0,
+      1, 0, 0],
+      [0, 1, 0,
+      0, 1, 0,
+      0, 1, 0],
+      [0, 0, 1,
+      0, 0, 1,
+      0, 0, 1],
+      [1, 0, 0,
+      0, 1, 0,
+      0, 0, 1],
+      [0, 0, 1,
+      0, 1, 0,
+      1, 0, 0]
+    ];
+
+    const situation = squares.map((square) => {
+      if (square.innerHTML === player) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    let winner;
+
+    combinations.forEach((combination) => {
+      
+      console.log(situation)
+      console.log(combination)
+
+      console.log(situation === combination)
+
+      if (situation === combination) {
+        console.log("winner");
+        winner = true;
+      }
+    });
+    return winner;
+  }
 };
