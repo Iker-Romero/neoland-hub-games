8fa2666a7b0c250365c52d1cc725253925bcac9e
Iker-Romero <ikersoftdev@gmail.com>
2022-10-17 12:04:40 +0200

    pokemon insert function in process

--- components/poke-api/poke-api.js
+++ components/poke-api/poke-api.js
@@ -1,20 +1,56 @@
 import { getData } from "../../services/sevices";
 
-export const pokeAPI = () => {
-  const template = `
-  <main>
-    <h1>POKE API</h1>
-  </main>
-  `;
+export const pokeAPI = async () => {
+  try {
+    const template = `
+      <main>
+        <h1>POKE API</h1>
+      </main>
+      `;
 
-  const target = document.querySelector("header");
-  target.insertAdjacentHTML("afterend", template);
+    const target = document.querySelector("header");
+    target.insertAdjacentHTML("afterend", template);
 
-  const pokemonsIndex = getData(
-    "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0"
-  );
+    const pokemonsIndex = await getData(
+      "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0"
+    );
 
-  pokemonsIndex.results.forEach((pokemonEntrance) => {
-    const pokemonURL = pokemonEntrance.url;
-  });
+    pokemonsIndex.results.forEach(async (pokemonEntrance) => {
+      try {
+        const pokemonURL = pokemonEntrance.url;
+        const pokemon = await getData(pokemonURL);
+        const figure = `
+          <figure>
+            <figcaption>${name}</figcaption>
+            <img src="${defaultIMG}" alt="Default pokemon image" class="default-image">
+            <img src="${shinyIMG}" alt="Shiny pokemon image" class="shiny-image">
+            <div class="types">
+
+            </div>
+          </figure>
+          `;
+        const {
+          name,
+          sprites: {
+            other: {
+              home: { front_default: defaultIMG, front_hiny: shinyIMG },
+            },
+          },
+          // types.forEach((type) => {
+          // }):
+          types,
+        } = pokemon;
+
+        const typesDiv = document.querySelector(".types")
+        types.forEach((slot) => {
+          const {type} = slot
+          typesDiv.insertAdjacentHTML("afterbegin", type);
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
