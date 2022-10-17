ce124bbfa9997fdf08dab631912cfe2176df9e13
Iker-Romero <ikersoftdev@gmail.com>
2022-10-17 12:53:26 +0200

    pokemons insertion working

--- components/poke-api/poke-api.js
+++ components/poke-api/poke-api.js
@@ -5,6 +5,9 @@ export const pokeAPI = async () => {
     const template = `
       <main>
         <h1>POKE API</h1>
+        <div class="pokemons-container">
+        
+        </div>
       </main>
       `;
 
@@ -15,20 +18,34 @@ export const pokeAPI = async () => {
       "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0"
     );
 
+    const typesColors = {
+      bug: "#ABB842",
+      dark: "#6C594A",
+      dragon: "#6D48D2",
+      electric: "F2D153",
+      fairy: "#ECA4DD",
+      fight: "#85372D",
+      fire: "#DE5C3D",
+      flying: "##A491EA",
+      ghost: "#6C5994",
+      grass: "#8BC660",
+      ground: "#DBC174",
+      ice: "#A6D6D7",
+      normal: "#A8A87D",
+      poison: "#97489C",
+      psychic: "#E66388",
+      rock: "#B4A14A",
+      shadow: "#3E3345",
+      steel: "#B8B8CE",
+      uknown: "#B8B8CE",
+      water: "#B8B8CE"
+    };
+
     pokemonsIndex.results.forEach(async (pokemonEntrance) => {
       try {
         const pokemonURL = pokemonEntrance.url;
         const pokemon = await getData(pokemonURL);
-        const figure = `
-          <figure>
-            <figcaption>${name}</figcaption>
-            <img src="${defaultIMG}" alt="Default pokemon image" class="default-image">
-            <img src="${shinyIMG}" alt="Shiny pokemon image" class="shiny-image">
-            <div class="types">
-
-            </div>
-          </figure>
-          `;
+        
         const {
           name,
           sprites: {
@@ -41,28 +58,19 @@ export const pokeAPI = async () => {
           types,
         } = pokemon;
 
-        typesColors = {
-          bug: "#ABB842",
-          dark: "#6C594A",
-          dragon: "#6D48D2",
-          electric: "F2D153",
-          fairy: "#ECA4DD",
-          fight: "#85372D",
-          fire: "#DE5C3D",
-          flying: "##A491EA",
-          ghost: "#6C5994",
-          grass: "#8BC660",
-          ground: "#DBC174",
-          ice: "#A6D6D7",
-          normal: "#A8A87D",
-          poison: "#97489C",
-          psychic: "#E66388",
-          rock: "#B4A14A",
-          shadow: "#3E3345",
-          steel: "#B8B8CE",
-          uknown: "#B8B8CE",
-          water: "#B8B8CE"
-        };
+        const figure = `
+          <figure>
+            <figcaption>${name}</figcaption>
+            <img src="${defaultIMG}" alt="Default pokemon image" class="default-image">
+            <img src="${shinyIMG}" alt="Shiny pokemon image" class="shiny-image">
+            <div class="types">
+
+            </div>
+          </figure>
+          `;
+
+        const pokemonsContainer = document.querySelector(".pokemons-container")
+        pokemonsContainer.insertAdjacentHTML("afterbegin", figure);
 
         const typesDiv = document.querySelector(".types");
         types.forEach((slot) => {
@@ -70,10 +78,11 @@ export const pokeAPI = async () => {
           typesDiv.insertAdjacentHTML(
             "afterbegin",
             `
-            <p>${type}</p>
+            <p style="backgroundColor: ${typesColors.type.name}>${type.name.toUpperCase()}</p>
             `
           );
         });
+
       } catch (error) {
         console.log(error);
       }
