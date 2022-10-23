4194a903362bf6f5ea6208bb3b06fc344777e85e
Iker-Romero <ikersoftdev@gmail.com>
2022-10-23 22:28:13 +0200

    first page trigger replaced by hub()

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { changeTheme } from "./components/change-theme/change-theme";
 import { footer } from "./components/footer/footer";
-import { login } from "./pages/login/login";
+import { hub } from "./pages/hub/hub";
 import { nav } from "./components/nav/nav";
 import { randomColor } from "./components/random-color/random-color";
 import "./style.css";
@@ -9,7 +9,7 @@ const init = () => {
   nav()
   randomColor();
   changeTheme();
-  login();
+  hub();
   footer();
 };
 
